749eb4e6fdb80f10c05d093137204e4199bf24b5
erpmario <34783705+erpmario@users.noreply.github.com>
2022-12-06 14:47:52 -0600

    Test commit

--- Determinant.c
+++ Determinant.c
@@ -3,111 +3,115 @@
 #include <math.h>
 
 
-#define ARRAYSIZE 16
+#define ARRAY_SIZE 16
 
 int determinantOfMatrix(int* mat, int n);
-void swap(int *p,int *q);
+void swap(int* p, int* q);
 
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    char f_name[50];
-    double a[ARRAYSIZE][ARRAYSIZE];
-    int i,j;
-    double det;
-    //Create filename
-    sprintf(f_name,"DET_DATA/m0032x0032.bin");
-    printf("Reading array file %s of size %dx%d\n",f_name,ARRAYSIZE,ARRAYSIZE);
-    //Open file
-    FILE *datafile=fopen(f_name,"rb");
-    //Read elelements
-    for (i=0; i< ARRAYSIZE; i++)
-        for (j=0; j< ARRAYSIZE; j++)
-        {
-            fread(&a[i][j],sizeof(double),1,datafile);
-            printf("a[%d][%d]=%f\n",i,j,a[i][j]);
-        }
-    printf("Matrix has been read.\n");
+	char fileName[50];
+	double a[ARRAY_SIZE][ARRAY_SIZE];
+	int i, j;
+	double det;
+	// Create filename
+	sprintf(fileName, "DET_DATA/m0032x0032.bin");
+	printf("Reading array file %s of size %dx%d\n", fileName, ARRAY_SIZE, ARRAY_SIZE);
+	// Open file
+	FILE* dataFile = fopen(fileName, "rb");
+	// Read elements
+	for(i = 0; i < ARRAY_SIZE; i++)
+	{
+		for(j = 0; j < ARRAY_SIZE; j++)
+		{
+			fread(&a[i][j], sizeof(double), 1, dataFile);
+			printf("a[%d][%d]=%f\n", i, j, a[i][j]);
+		}
+	}
+	printf("Matrix has been read.\n");
 
-    //Calculate determinant
+	// Calculate determinant
 
 }
+
 
 int determinantOfMatrix(int* mat, int n)
 {
-    int num1, num2, det = 1, index,
-            total = 1; // Initialize result
+	int num1, num2, det = 1, index,
+			total = 1; // Initialize result
 
-    // temporary array for storing row
-    int temp[n + 1];
+	// temporary array for storing row
+	int temp[n + 1];
 
-    // loop for traversing the diagonal elements
-    for (int i = 0; i < n; i++)
-    {
-        index = i; // initialize the index
+	// loop for traversing the diagonal elements
+	for(int i = 0; i < n; i++)
+	{
+		index = i; // initialize the index
 
-        // finding the index which has non zero value
-        while (index < n && mat[index * ARRAYSIZE + i] == 0)
-        {
-            index++;
-        }
-        if (index == n) // if there is non zero element
-        {
-            // the determinant of matrix as zero
-            continue;
-        }
-        if (index != i)
-        {
-            // loop for swapping the diagonal element row and
-            // index row
-            for (int j = 0; j < n; j++)
-            {
-                swap(&mat[index *ARRAYSIZE + j], &mat[i * ARRAYSIZE + j]);
-            }
-            // determinant sign changes when we shift rows
-            // go through determinant properties
-            det = det * pow(-1, index - i);
-        }
+		// finding the index which has non zero value
+		while(index < n && mat[index * ARRAY_SIZE + i] == 0)
+		{
+			index++;
+		}
+		if(index == n) // if there is non zero element
+		{
+			// the determinant of matrix as zero
+			continue;
+		}
+		if(index != i)
+		{
+			// loop for swapping the diagonal element row and
+			// index row
+			for(int j = 0; j < n; j++)
+			{
+				swap(&mat[index * ARRAY_SIZE + j], &mat[i * ARRAY_SIZE + j]);
+			}
+			// determinant sign changes when we shift rows
+			// go through determinant properties
+			det = det * pow(-1, index - i);
+		}
 
-        // storing the values of diagonal row elements
-        for (int j = 0; j < n; j++)
-        {
-            temp[j] = mat[i * ARRAYSIZE + j];
-        }
-        // traversing every row below the diagonal element
-        for (int j = i + 1; j < n; j++)
-        {
-            num1 = temp[i]; // value of diagonal element
-            num2 = mat[j * ARRAYSIZE + i]; // value of next row element
+		// storing the values of diagonal row elements
+		for(int j = 0; j < n; j++)
+		{
+			temp[j] = mat[i * ARRAY_SIZE + j];
+		}
+		// traversing every row below the diagonal element
+		for(int j = i + 1; j < n; j++)
+		{
+			num1 = temp[i]; // value of diagonal element
+			num2 = mat[j * ARRAY_SIZE + i]; // value of next row element
 
-            // traversing every column of row
-            // and multiplying to every row
-            for (int k = 0; k < n; k++)
-            {
-                // multiplying to make the diagonal
-                // element and next row element equal
-                mat[j * ARRAYSIZE + k]
-                        = (num1 * mat[j * ARRAYSIZE + k]) - (num2 * temp[k]);
-            }
-            total = total * num1; // Det(kA)=kDet(A);
-        }
-    }
+			// traversing every column of row
+			// and multiplying to every row
+			for(int k = 0; k < n; k++)
+			{
+				// multiplying to make the diagonal
+				// element and next row element equal
+				mat[j * ARRAY_SIZE + k]
+						= (num1 * mat[j * ARRAY_SIZE + k]) - (num2 * temp[k]);
+			}
+			total = total * num1; // Det(kA)=kDet(A);
+		}
+	}
 
-    // multiplying the diagonal elements to get determinant
-    for (int i = 0; i < n; i++)
-    {
-        det = det * mat[i * ARRAYSIZE + i];
-    }
-    return (det / total); // Det(kA)/k=Det(A);
+	// multiplying the diagonal elements to get determinant
+	for(int i = 0; i < n; i++)
+	{
+		det = det * mat[i * ARRAY_SIZE + i];
+	}
+	return (det / total); // Det(kA)/k=Det(A);
 }
 
-void swap(int *p,int *q)
-{
-    //p=&n1 so p store the address of n1, so *p store the value of n1
-    //q=&n2 so q store the address of n2, so *q store the value of n2
 
-    int tmp;
-    tmp = *p; // tmp store the value of n1
-    *p=*q;    // *p store the value of *q that is value of n2
-    *q=tmp;   // *q store the value of tmp that is the value of n1
+void swap(int* p, int* q)
+{
+	//p=&n1 so p store the address of n1, so *p store the value of n1
+	//q=&n2 so q store the address of n2, so *q store the value of n2
+
+	int tmp;
+	tmp = *p; // tmp store the value of n1
+	*p = *q;    // *p store the value of *q that is value of n2
+	*q = tmp;   // *q store the value of tmp that is the value of n1
 }
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Grendel/CLionProjects/CS-4373-Final-Project
+# Install script for directory: F:/CLionProjects/CS-4373-Final-Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/Program Files/CLion 2022.2.4/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/erpma/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/223.7571.171/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Grendel/CLionProjects/CS-4373-Final-Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "F:/CLionProjects/CS-4373-Final-Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
