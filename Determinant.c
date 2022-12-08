783e9270715cf47ed3a90352bb25a3cb6283a060
erpmario <34783705+erpmario@users.noreply.github.com>
2022-12-08 10:29:33 -0600

    Resolve merge conflicts in Determinant.c

--- Determinant.c
+++ Determinant.c
@@ -3,16 +3,16 @@
 #include <math.h>
 
 
-#define ARRAYSIZE 16
+#define ARRAY_SIZE 16
 
-double determinantOfMatrix(double * mat, int n);
-void swap(double *p,double *q);
+double determinantOfMatrix(double* mat, int n);
+double logDeterminantOfMatrix(double* mat, int n);
 
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	char fileName[50];
-	double* a = calloc(ARRAY_SIZE * ARRAY_SIZE, sizeof(double));
+	double* matrix = calloc(ARRAY_SIZE * ARRAY_SIZE, sizeof(double));
 	int i, j;
 	double det;
 	// Create filename
@@ -25,15 +25,17 @@ int main(int argc, char *argv[])
 	{
 		for(j = 0; j < ARRAY_SIZE; j++)
 		{
-			fread(&a[i * ARRAY_SIZE + j], sizeof(double), 1, datafile);
-			printf("a[%d][%d]=%f\n", i, j, a[i * ARRAY_SIZE + j]);
+			fread(&matrix[i * ARRAY_SIZE + j], sizeof(double), 1, datafile);
+			printf("matrix[%d][%d]=%f\n", i, j, matrix[i * ARRAY_SIZE + j]);
 		}
 	}
 	printf("Matrix has been read.\n");
 
-    //Calculate determinant
-    det = determinantOfMatrix(a,ARRAYSIZE);
-    printf("Determinant of the matrix is: %f\n",det);
+	//Calculate determinant
+	det = determinantOfMatrix(matrix, ARRAY_SIZE);
+	printf("Determinant of the matrix is: %f\n", det);
+	double logDet = logDeterminantOfMatrix(matrix, ARRAY_SIZE);
+	printf("Log determinant of the matrix is: %f\n", logDet);
 }
 
 
@@ -67,6 +69,42 @@ double determinantOfMatrix(double* mat, int n)
 	for(i = 0; i < n; i++)
 	{
 		det = det * mat[i * n + i];
+	}
+
+	return det;
+}
+
+
+double logDeterminantOfMatrix(double* mat, int n)
+{
+	int i, j, k;
+	double det = 0;
+	double ratio;
+
+	/* Applying Gauss Elimination */
+	for(i = 0; i < n; i++)
+	{
+		if(mat[i * n + i] == 0.0)
+		{
+			printf("Mathematical Error!");
+			exit(0);
+		}
+		for(j = i + 1; j < n; j++)
+		{
+			ratio = mat[j * n + i] / mat[i * n + i];
+
+			for(k = 0; k < n; k++)
+			{
+				mat[j * n + k] = mat[j * n + k] - ratio * mat[i * n + k];
+			}
+		}
+	}
+
+	/* Finding determinant by multiplying
+	 elements in principal diagonal elements */
+	for(i = 0; i < n; i++)
+	{
+		det = det + log10(fabs(mat[i * n + i]));
 	}
 
 	return det;
